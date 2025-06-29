d9f4a63b5c8aa50d2280c120642fcab6c2ab9ce8
Varun Raj <varunraj1545@gmail.com>
2025-06-29 14:26:25 +0530

    render movies cards

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Maincontainer from "./Maincontainer";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies"; // ✅ import the hook
+import SecondaryContainer from "./SecondaryContainer";
 
 const BrowsePage = () => {
   useNowPlayingMovies(); // ✅ call the hook to fetch movies
@@ -10,6 +11,7 @@ const BrowsePage = () => {
     <>
       <Header />
       <Maincontainer />
+      <SecondaryContainer/>
     </>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,6 @@ export const OPTION = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjdkNjczY2JlNjJmOTM5NDExY2JhNTllOWY0NjE3MyIsIm5iZiI6MTcyNTg1NDI5My4wNDcwMDAyLCJzdWIiOiI2NmRlNzI1NWMxMDg5MjIyZWJlYzkxMTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZJzYeV7K3YJ5qKw8LNgRWQYXemaF2UCnWLvwTqmeJuY'
   }
 };
+
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
