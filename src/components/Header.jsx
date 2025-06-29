e1ab11d2289f718407fae580ef0d9e1079a6dd93
Varun Raj <varunraj1545@gmail.com>
2025-06-29 13:53:13 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,31 +38,32 @@ const Header = () => {
   }, [dispatch, navigate]);
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
-      <div className="p-2 inline-block ml-5">
-        <img
-          className="w-32 h-14 drop-shadow-[0_0_2px_white] shadow-lg"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Cineverse_logo.svg/1200px-Cineverse_logo.svg.png"
-          alt="logo"
-        />
-      </div>
+   <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+  <div className="p-2 ml-5">
+    <img
+      className="w-32 h-14 drop-shadow-[0_0_2px_white] shadow-lg"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Cineverse_logo.svg/1200px-Cineverse_logo.svg.png"
+      alt="logo"
+    />
+  </div>
 
-      {user && (
-        <div className="flex items-center mt-2 md:mt-0">
-          <img
-            className="w-8 h-10 mr-2 rounded-xl"
-            src="https://occ-0-4345-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e"
-            alt="user"
-          />
-          <button
-            onClick={HandleSignout}
-            className="mr-5 text-sm bg-red-500 hover:bg-red-600 h-8 px-4 rounded-xl font-bold cursor-pointer"
-          >
-            Signout
-          </button>
-        </div>
-      )}
+  {user && (
+    <div className="flex p-2 justify-between">
+      <img
+        className="hidden md:block w-8 h-10 mr-2 rounded-xl"
+        src="https://occ-0-4345-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e"
+        alt="user"
+      />
+      <button
+        onClick={HandleSignout}
+        className="font-bold text-white text-sm bg-red-500 hover:bg-red-600 h-8 px-4 rounded-xl cursor-pointer"
+      >
+        Signout
+      </button>
     </div>
+  )}
+</div>
+
   );
 };
 
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -9,13 +9,16 @@ const VideoBackground = ({ movieId }) => {
   if (!trailerVideo) return null;
 
   return (
-    <div className="w-screen h-screen">
+    <div className=" w-screen">
       <iframe
-        className="w-screen h-full"
-        src={`https://www.youtube.com/embed/${trailerVideo.key}?&autoplay=1&mute=1`}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
       ></iframe>
     </div>
   );
